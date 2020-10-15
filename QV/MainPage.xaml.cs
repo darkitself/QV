5dd068585457742d513d01cca8864579f1d3010c
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2020-10-15 21:53:08 +0500

    Add QrCode Library

--- QV/MainPage.xaml.cs
+++ QV/MainPage.xaml.cs
@@ -4,15 +4,26 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QV.Infrastructure;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using ZXing.Net.Mobile.Forms;
+using ZXing.QrCode;
 
 namespace QV
 {
     public partial class MainPage : ContentPage
     {
+        public Action<object, EventArgs> handler { get; set; }
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            handler(sender, e);
         }
     }
 }
