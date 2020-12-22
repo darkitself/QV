a28c9f103c4369d5e0f20fcf345b40f406864caa
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2020-12-22 20:38:04 +0500

    Fix contact saver

--- QV.Android/ContactSaver.cs
+++ QV.Android/ContactSaver.cs
@@ -1,10 +1,12 @@
-﻿
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Provider;
+using Android.Widget;
 using QV.Droid;
 using QV.Infrastructure;
-[assembly:Xamarin.Forms.Dependency(typeof(ContactSaver))]
+
+[assembly: UsesPermission(Android.Manifest.Permission.ReadContacts)]
+[assembly: Xamarin.Forms.Dependency(typeof(ContactSaver))]
 
 namespace QV.Droid
 {
@@ -12,14 +14,58 @@ namespace QV.Droid
     {
         public void SaveContact(UserData userData)
         {
+            if (IsContactsContainNumber(userData.Phone_Number))
+            {
+                Toast.MakeText(Application.Context, "Такой контакт уже существует",
+                               ToastLength.Long)?.Show();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(userData.Phone_Number))
+            {
+                Toast.MakeText(Application.Context, "У визитки нет номера телефона",
+                               ToastLength.Long)?.Show();
+                return;
+            }
+
+            StartContactSaveActivity(userData);
+        }
+
+        private void StartContactSaveActivity(UserData userData)
+        {
             var intent = new Intent(Intent.ActionInsert);
             intent.SetFlags(ActivityFlags.NewTask);
             intent.SetType(ContactsContract.Contacts.ContentType);
-            intent.PutExtra(ContactsContract.Intents.Insert.Name,$"{userData.Surname} {userData.Name} {userData.Patronymic}");
+            intent.PutExtra(ContactsContract.Intents.Insert.Name,
+                            $"{userData.Surname} {userData.Name} {userData.Patronymic}");
             intent.PutExtra(ContactsContract.Intents.Insert.Email, userData.Email);
             intent.PutExtra(ContactsContract.Intents.Insert.Phone, userData.Phone_Number);
             intent.PutExtra(ContactsContract.Intents.Insert.Notes, userData.Info);
             Application.Context.StartActivity(intent);
+        }
+
+        private bool IsContactsContainNumber(string phoneNumber)
+        {
+            using var phones =
+                Application.Context.ContentResolver.Query(
+                                                          ContactsContract.CommonDataKinds.Phone
+                                                              .ContentUri,
+                                                          null,
+                                                          ContactsContract.CommonDataKinds.Phone
+                                                              .InterfaceConsts.DisplayName,
+                                                          null,
+                                                          null);
+            if (phones == null) return false;
+            while (phones.MoveToNext())
+            {
+                var number =
+                    phones.GetString(phones.GetColumnIndex(ContactsContract.CommonDataKinds.Phone
+                                                               .Number));
+                if (number == null || !number.Equals(phoneNumber)) continue;
+                return true;
+            }
+
+            return false;
         }
     }
 }
--- QV.Android/MainActivity.cs
+++ QV.Android/MainActivity.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Android;
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
@@ -14,12 +15,14 @@ using Xamarin.Essentials;
 
 namespace QV.Droid
 {
+    
     [Activity(Label = "QV", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, 
                  ConfigurationChanges = ConfigChanges.ScreenSize 
                                         | ConfigChanges.Orientation 
                                         | ConfigChanges.UiMode 
                                         | ConfigChanges.ScreenLayout 
                                         | ConfigChanges.SmallestScreenSize )]
+    
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         internal static MainActivity mainActivity { get; private set; }
@@ -32,18 +35,18 @@ namespace QV.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Platform.Init(this, savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
             mainActivity = this;
             var app = new App();
             LoadApplication(app);
+            RequestPermissions(new [] { Manifest.Permission.ReadContacts}, 0);
         }
         
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+           Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+           base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
         
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
